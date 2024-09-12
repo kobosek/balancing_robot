29f404c3bf4b4848b3bb975cc636891a5f976ec4
Karol Lewicki <klewicki9@gmail.com>
2024-09-12 20:15:14 +0200

    logging update

--- main/Logger.cpp
+++ main/Logger.cpp
@@ -1,20 +1,22 @@
 #include "include/Logger.hpp"
 
 Logger::LogLevel Logger::globalLogLevel = Logger::LogLevel::INFO;
-std::unordered_map<std::string, Logger::LogLevel> Logger::componentLogLevels;
+std::unordered_map<std::string, Logger::LogLevel> Logger::tagLevels;
 
 void Logger::setGlobalLogLevel(LogLevel level) {
     globalLogLevel = level;
+    esp_log_level_set("*", static_cast<esp_log_level_t>(level));
 }
 
-void Logger::setComponentLogLevel(const std::string& component, LogLevel level) {
-    componentLogLevels[component] = level;
+void Logger::setLogLevel(const char* tag, LogLevel level) {
+    tagLevels[tag] = level;
+    esp_log_level_set(tag, static_cast<esp_log_level_t>(level));
 }
 
-Logger::LogLevel Logger::getEffectiveLogLevel(const char* tag) {
-    auto it = componentLogLevels.find(tag);
-    if (it != componentLogLevels.end()) {
-        return it->second;
+bool Logger::shouldLog(esp_log_level_t level, const char* tag) {
+    auto it = tagLevels.find(tag);
+    if (it != tagLevels.end()) {
+        return level <= static_cast<esp_log_level_t>(it->second);
     }
-    return globalLogLevel;
+    return level <= static_cast<esp_log_level_t>(globalLogLevel);
 }
--- main/include/Logger.hpp
+++ main/include/Logger.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "esp_err.h"
 #include "esp_log.h"
 #include <string>
@@ -7,66 +8,53 @@
 class Logger {
 public:
     enum class LogLevel {
-        ERROR,
-        WARN,
-        INFO,
-        DEBUG,
-        VERBOSE
+        NONE = ESP_LOG_NONE,
+        ERROR = ESP_LOG_ERROR,
+        WARN = ESP_LOG_WARN,
+        INFO = ESP_LOG_INFO,
+        DEBUG = ESP_LOG_DEBUG,
+        VERBOSE = ESP_LOG_VERBOSE
     };
 
     static void setGlobalLogLevel(LogLevel level);
-    static void setComponentLogLevel(const std::string& component, LogLevel level);
-
-    template<typename... Args>
-    static void log(const char* tag, LogLevel level, const char* format, Args... args) {
-        if (level > getEffectiveLogLevel(tag)) return;
-
-        switch (level) {
-            case LogLevel::ERROR:
-                ESP_LOGE(tag, format, args...);
-                break;
-            case LogLevel::WARN:
-                ESP_LOGW(tag, format, args...);
-                break;
-            case LogLevel::INFO:
-                ESP_LOGI(tag, format, args...);
-                break;
-            case LogLevel::DEBUG:
-                ESP_LOGD(tag, format, args...);
-                break;
-            case LogLevel::VERBOSE:
-                ESP_LOGV(tag, format, args...);
-                break;
-        }
-    }
+    static void setLogLevel(const char* tag, LogLevel level);
 
     template<typename... Args>
     static void error(const char* tag, const char* format, Args... args) {
-        log(tag, LogLevel::ERROR, format, args...);
+        log(ESP_LOG_ERROR, tag, format, args...);
     }
 
     template<typename... Args>
     static void warn(const char* tag, const char* format, Args... args) {
-        log(tag, LogLevel::WARN, format, args...);
+        log(ESP_LOG_WARN, tag, format, args...);
     }
 
     template<typename... Args>
     static void info(const char* tag, const char* format, Args... args) {
-        log(tag, LogLevel::INFO, format, args...);
+        log(ESP_LOG_INFO, tag, format, args...);
     }
 
     template<typename... Args>
     static void debug(const char* tag, const char* format, Args... args) {
-        log(tag, LogLevel::DEBUG, format, args...);
+        log(ESP_LOG_DEBUG, tag, format, args...);
     }
 
     template<typename... Args>
     static void verbose(const char* tag, const char* format, Args... args) {
-        log(tag, LogLevel::VERBOSE, format, args...);
+        log(ESP_LOG_VERBOSE, tag, format, args...);
     }
 
 private:
     static LogLevel globalLogLevel;
-    static std::unordered_map<std::string, LogLevel> componentLogLevels;
-    static LogLevel getEffectiveLogLevel(const char* tag);
+    static std::unordered_map<std::string, LogLevel> tagLevels;
+
+    template<typename... Args>
+    
+    static void log(esp_log_level_t level, const char* tag, const char* format, Args... args) {
+        if (shouldLog(level, tag)) {
+            esp_log_write(level, tag, format, args...);
+        }
+    }
+
+    static bool shouldLog(esp_log_level_t level, const char* tag);
 };
